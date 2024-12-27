bda94d5f198392cc300b97644e0a78d1731054e0
emilie <emilie.wermelinger@etu.hepl.ch>
2024-12-27 13:08:20 +0100

    indentation

--- src/components/QuizForm.vue
+++ src/components/QuizForm.vue
@@ -74,107 +74,107 @@ onMounted(() => {
         ]"
         answer-detail="Une année bissextile comporte 366 jours et une non bissextile 365"
       />
-    <QuestionRadio
-      v-if="index===1"
-      id="hep"
-      v-model="questionStates[1]"
-      answer="9"
-      text="Combien y a-t-il de bâtiments de la Hep à l'avenue de Cour 33?"
-      :options="[
-        { value: '9', text: '9' },
-        { value: '8', text: '8' },
-        { value: '10', text: '10' },
-        { value: '7', text: '7' },
-      ]"
-      answer-detail="La Hep a 9 bâtiments en comptant l'aula"
-    />
-    <QuestionRadio
-      v-if="index===2"
-      id="titi"
-      v-model="questionStates[2]"
-      answer="titi"
-      text=" Quelle est le nom de l'oiseau que Gros-minet veut manger ?"
-      :options="[
-        { value: 'titi', text: 'Titi' },
-        { value: 'Lulu', text: 'Lulu' },
-        { value: 'momo', text: 'Momo' },
-        { value: 'fafa', text: 'Fafa' },
-      ]"
-    />
-
-    <QuestionText
-      id="chat"
-      v-model="questionStates[3]"
-      :answer="['4','quatre']"
-      text="Combien de pattes a un chat ?"
-      answer-detail="Le chat est un mammifère quadrupède."
-    />
-
-    <QuestionText
-      id="bouriquet"
-      v-model="questionStates[4]"
-      :answer="['bouriquet', 'Bouriquet','bourriquet','Bourriquet']"
-      text="Comment se nomme l'âne dans winnie l'ourson ?"
-      answer-detail="Il s'appelle Bourriquet"
+      <QuestionRadio
+        v-if="index===1"
+        id="hep"
+        v-model="questionStates[1]"
+        answer="9"
+        text="Combien y a-t-il de bâtiments de la Hep à l'avenue de Cour 33?"
+        :options="[
+          { value: '9', text: '9' },
+          { value: '8', text: '8' },
+          { value: '10', text: '10' },
+          { value: '7', text: '7' },
+        ]"
+        answer-detail="La Hep a 9 bâtiments en comptant l'aula"
+      />
+      <QuestionRadio
+        v-if="index===2"
+        id="titi"
+        v-model="questionStates[2]"
+        answer="titi"
+        text=" Quelle est le nom de l'oiseau que Gros-minet veut manger ?"
+        :options="[
+          { value: 'titi', text: 'Titi' },
+          { value: 'Lulu', text: 'Lulu' },
+          { value: 'momo', text: 'Momo' },
+          { value: 'fafa', text: 'Fafa' },
+        ]"
       />
 
-    <QuestionSelect
-      id="chien"
-      v-model="questionStates[5]"
-      answer="chien"
-      text="Quel est l'animal qui représente Snoopy ?"
-      :options="[
-        { value: 'chien', text: 'Chien' },
-        { value: 'chat', text: 'Chat' },
-        { value: 'souris', text: 'Souris' },
-        { value: 'oiseau', text: 'Oiseau' }
-      ]"
-      answer-detail="Snoopy est un chien noir et blanc"
-    />
+      <QuestionText
+        id="chat"
+        v-model="questionStates[3]"
+        :answer="['4','quatre']"
+        text="Combien de pattes a un chat ?"
+        answer-detail="Le chat est un mammifère quadrupède."
+      />
 
-    <QuestionSelect
-      id="garfield"
-      v-model="questionStates[6]"
-      answer="orange"
-      text="De quelle couleur est Garfield ?"
-      :options="[
-        { value: 'bleu', text: 'Bleu' },
-        { value: 'orange', text: 'Orange' },
-        { value: 'jaune', text: 'Jaune' },
-        { value: 'violet', text: 'Violet' }
-      ]"
-      answer-detail="Garfield est un chat orange qui adore les lasagnes"
-    />
+      <QuestionText
+        id="bouriquet"
+        v-model="questionStates[4]"
+        :answer="['bouriquet', 'Bouriquet','bourriquet','Bourriquet']"
+        text="Comment se nomme l'âne dans winnie l'ourson ?"
+        answer-detail="Il s'appelle Bourriquet"
+      />
 
-    <QuestionSelect
-      v-if="index===7"
-      id="winnie"
-      v-model="questionStates[7]"
-      answer="pense"
-      text="Que dit winnie à haute voix en se cognant la tête ?"
-      :options="[
-        { value: 'pense', text: 'Pense, pense, pense' },
-        { value: 'réflechit', text: 'Réflechit, réflechit, réflechit' },
-        { value: 'concentrer', text: 'Concentres-toi' },
-        { value: 'souvenir', text: 'Souviens-toi' }
-      ]"
-      answer-detail="Winnie dit Pense, pense, pense"
-    />
+      <QuestionSelect
+        id="chien"
+        v-model="questionStates[5]"
+        answer="chien"
+        text="Quel est l'animal qui représente Snoopy ?"
+        :options="[
+          { value: 'chien', text: 'Chien' },
+          { value: 'chat', text: 'Chat' },
+          { value: 'souris', text: 'Souris' },
+          { value: 'oiseau', text: 'Oiseau' }
+        ]"
+        answer-detail="Snoopy est un chien noir et blanc"
+      />
 
-    <QuestionCheckbox
-      v-if="index===8"
-      id="ecole"
-      v-model="questionStates[8]"
-      :answer="['Allemand', 'Français','Anglais']"
-      text="Quelles sont les langues obligatoires apprises à l'école en Suisse romande ?"
-      :options="[
-        { value: 'Allemand', text: 'Allemand' },
-        { value: 'Français', text: 'Français' },
-        { value: 'Anglais', text: 'Anglais' },
-        { value: 'Italien', text: 'Italien' },
-      ]"
-      answer-detail="Les élèves apprennent trois langues principales, l'Allemand, le Français et l'Anglais et l'Italien est en option"
-    />
+      <QuestionSelect
+        id="garfield"
+        v-model="questionStates[6]"
+        answer="orange"
+        text="De quelle couleur est Garfield ?"
+        :options="[
+          { value: 'bleu', text: 'Bleu' },
+          { value: 'orange', text: 'Orange' },
+          { value: 'jaune', text: 'Jaune' },
+          { value: 'violet', text: 'Violet' }
+        ]"
+        answer-detail="Garfield est un chat orange qui adore les lasagnes"
+      />
+
+      <QuestionSelect
+        v-if="index===7"
+        id="winnie"
+        v-model="questionStates[7]"
+        answer="pense"
+        text="Que dit winnie à haute voix en se cognant la tête ?"
+        :options="[
+          { value: 'pense', text: 'Pense, pense, pense' },
+          { value: 'réflechit', text: 'Réflechit, réflechit, réflechit' },
+          { value: 'concentrer', text: 'Concentres-toi' },
+          { value: 'souvenir', text: 'Souviens-toi' }
+        ]"
+        answer-detail="Winnie dit Pense, pense, pense"
+      />
+
+      <QuestionCheckbox
+        v-if="index===8"
+        id="ecole"
+        v-model="questionStates[8]"
+        :answer="['Allemand', 'Français','Anglais']"
+        text="Quelles sont les langues obligatoires apprises à l'école en Suisse romande ?"
+        :options="[
+          { value: 'Allemand', text: 'Allemand' },
+          { value: 'Français', text: 'Français' },
+          { value: 'Anglais', text: 'Anglais' },
+          { value: 'Italien', text: 'Italien' },
+        ]"
+        answer-detail="Les élèves apprennent trois langues principales, l'Allemand, le Français et l'Anglais et l'Italien est en option"
+      />
     </div>
 
 
