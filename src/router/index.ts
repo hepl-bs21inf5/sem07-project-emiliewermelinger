990b099e29083c493a92963fa46b68710d425b71
emilie <emilie.wermelinger@etu.hepl.ch>
2024-12-27 12:26:15 +0100

    testhistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import LausanneView from '../views/LausanneView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/sem07-project-emiliewermelinger/'),
   routes: [
     {
       path: '/',
@@ -23,9 +23,6 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
   ],
